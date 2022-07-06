e4ab6c6becb80633788643dec0a488aa1434fa01
samaralielsayed <engsamar105@gmail.com>
2022-07-06 09:00:35 +0200

    Task2

---
+++ Task2/Task2.php
@@ -0,0 +1,128 @@
+<?php
+// dynamic table
+// dynamic rows //4 
+// dynamic columns // 4
+// check if gender of user == m ==> male // 1
+// check if gender of user == f ==> female // 1
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing',
+        ],
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+    ], 
+    
+];
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <?php foreach($users[0] AS $property=>$value){?>
+            <th scope="col"> <?php echo $property?></th> <?php } ?>
+        </thead>
+        <tbody>
+            <?php foreach($users AS $user) { ?>
+
+            <tr>
+                <?php foreach($user AS $value) {?>
+                <td><?php 
+                    $test_int=1;
+                    $test_string="string";
+                    $test_arr=[];
+                    $test_obj=(object)[];
+                    $found = false;
+
+                    if(gettype($test_int)==gettype($value) ||gettype($test_string)==gettype($value)){
+                        echo $value;
+                    }elseif(gettype($test_arr)==gettype($value)){
+                        foreach($value AS $values){
+                            echo " {$values} <br> ";
+                        }
+                    } elseif(gettype($test_obj)==gettype($value)){
+                        foreach($value AS $values){
+                              if($user->gender->gender =="f"){
+                                $found = true;
+                            }
+                        } 
+                        if($found){
+                            echo "female";
+                        }else{
+                            echo "male";
+                         }
+                    }        
+                        ?></td>
+                <?php } ?>
+
+            </tr>
+
+                 <?php } ?>
+
+        </tbody>
+    </table>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
